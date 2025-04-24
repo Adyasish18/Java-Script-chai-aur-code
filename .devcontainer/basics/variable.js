2ae9e16185e5c6d9774783d909a8c3e5a0c1d211
Adyasish18 <satpathyalok23@gmail.com>
2025-04-24 05:16:45 +0000

    container config done

---
+++ .devcontainer/basics/variable.js
@@ -0,0 +1,10 @@
+const accountid=14453
+let accountemail="Alok@google.com"
+var accountpassword="12345"
+accountcity="BBSR"
+// accountid=2// not allowed
+accountemail="Ady@google.com"
+accountpassword="212134"
+accountcity="jaipur"
+console.log(accountid)
+console.table([accountid,accountemail,accountpassword,accountcity])
