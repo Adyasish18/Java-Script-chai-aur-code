a18c4f6cfcb86a9f0a7aad19c53414401178989e
Adyasish18 <satpathyalok23@gmail.com>
2025-04-25 20:20:19 +0000

    container config done

---
+++ .devcontainer/basics/datatype.js
@@ -0,0 +1,11 @@
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
+// number=>2 to power 53
+// bigint
+// string=>" "
+// boolean=> true or false
+// null=> standalone value 
+// undefined=>doesnot know which type of value is store
+// symbol=>unique
+let x="alok";
+console.log(typeof x);
---
+++ .devcontainer/basics/declaration of object.js
@@ -0,0 +1,10 @@
+const std={
+    fullname:"Adyasish Satpathy",
+    Age:22,
+    Cgpa:8.3,
+    ispass:true
+};
+console.log(std);
+console.log("student age is:",std["Age"]);
+std.Age=9;
+console.log(std);
