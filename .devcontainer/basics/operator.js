cc8d4435a1a3aed5de94361a2f8b442ee6e94342
Adyasish18 <satpathyalok23@gmail.com>
2025-04-26 04:07:47 +0000

    container config done

---
+++ .devcontainer/basics/operator.js
@@ -0,0 +1,36 @@
+// Arithmatic operators
+// let a=5;
+// let b=2;
+// console.log("a+b=",a+b);
+// console.log("a-b=",a-b);
+// console.log("a*b=",a*b);
+// console.log("a%b=",a%b);
+// console.log("a/b=",a/b);
+// console.log("a**b=",a**b);
+// unary operators
+// let a=5;
+// let b=2;
+// console.log("a=",a,"& b=",b);
+// console.log("++a=",++a);
+// console.log("after pre increament the value of a is:",a);
+// console.log("a++=",a++);
+// console.log("after post increament the value of a is:",a);
+// comparision operator
+// let a=5;
+// let b=2;
+// console.log("5==2",a==b);
+// console.log("5!=2",a!=b);
+// console.log("5>=2",a>=b);
+// console.log("5<=2",a<=b);
+// let a=5;
+// let b="2";
+// console.log("a===b",a===b);
+// console.log("a!==b",a!==b);
+// Logical operator
+// let a=6;
+// let b=5;
+// let cond1 = a>b//true
+// let cond2 = a<b//false
+// console.log("cond1 && cond2:",cond1&&cond2);
+// console.log("cond1 || cond2:",cond1||cond2);
+// console.log("!(a<=b)",!(a<=b));//not false means true
