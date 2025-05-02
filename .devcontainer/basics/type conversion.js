5265fa2725ad51f946f13f13dc6654e8ea5492a8
Adyasish18 <satpathyalok23@gmail.com>
2025-05-02 03:15:10 +0000

    container config done

---
+++ .devcontainer/basics/type conversion.js
@@ -0,0 +1,15 @@
+// let score="33";
+// console.log(typeof score);
+// let value=Number(score);
+// console.log(typeof value);
+// console.log(value);
+// let score=true;
+// console.log(typeof score);
+// let value=Number(score);
+// console.log(typeof value);
+// console.log(value);
+let score="ALOK";
+console.log(typeof score);
+let value=Number(score);
+console.log(typeof value);
+console.log(value);
